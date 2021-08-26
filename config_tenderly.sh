6604779f36e5d26e5662a9597927c64b22bd036d
Dmitry <dymitr.paremski@keep.network>
2021-08-26 14:57:13 +0200

    Testing

--- config_tenderly.sh
+++ config_tenderly.sh
@@ -10,3 +10,5 @@ printf "${LOG_START}Configuring Tenderly...${LOG_END}"
 mkdir $HOME/.tenderly && touch $HOME/.tenderly/config.yaml
 
 echo access_key: ${TENDERLY_API_KEY} > $HOME/.tenderly/config.yaml
+
+cat $HOME/.tenderly/config.yaml
