ad9fb7f60d77e034d32bd26339537f8ee4ff2097
Dmitry <dymitr.paremski@keep.network>
2021-08-26 14:44:04 +0200

    Testing

--- config_tenderly.sh
+++ config_tenderly.sh
@@ -7,4 +7,6 @@ LOG_END='\n\e[0m' # new line + reset color
 
 printf "${LOG_START}Configuring Tenderly...${LOG_END}"
 
+touch $HOME/.tenderly/config.yaml
+
 echo access_key: ${TENDERLY_API_KEY} > $HOME/.tenderly/config.yaml
